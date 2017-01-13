b44e2167a1f35c9cc9eb4a50cee13dfdfa955945
Daniel Medina <ifthenelse42@protonmail.com>
2017-01-13 06:59:35 +0000

    rebuilding front-end

--- www/public/js/repository.js
+++ www/public/js/repository.js
@@ -1,77 +1,3 @@
-function liveAddForm(element, url, actionName, actionAttr, placeholder, prependTo) {
-    $(element).on('click', function() {
-	var csrf_token = $('meta[name="csrf-token"]').attr('content');
-	var form = '<form method="POST" action="' + url + '" class="form '+ actionName +'">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '">' + '<input type="text" class="form-control" name="'+ actionAttr +'" placeholder="'+ placeholder +'"></form>';
-
-	$(prependTo).first().prepend(form);
-    });
-}
-function liveEditForm(element, data, url, actionName, actionAttr, replaceAt) {
-    $(element).on('click', function() {
-	var id = $(this).attr(data);
-	var old = $(this).closest(replaceAt).text();
-	var csrf_token = $('meta[name="csrf-token"]').attr('content');
-	var form = '<form method="POST" data-attribute="'+ id +'" action="' + url + '" class="form '+ actionName +'">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '">' + '<input type="hidden" name="id" value="'+ id +'"><input type="text" class="form-control" name="'+ actionAttr +'" value="'+ old +'"></form>';
-	$(this).closest(replaceAt).replaceWith(form);
-    });
-}
-
-function liveExec(type, element, url, actionName, actionAttr, prependTo, newData) {
-    $(document).on('submit', element, function (e) {
-	if(type == "add") {
-	    e.preventDefault();
-	    var value = $(this).find('input[name='+ actionName +']').val();
-	    var deleteForm = $(this).remove();
-	
-	    $.ajax({
-		headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-		type: "POST",
-		url: url,
-		data: $(this).serialize()
-	    }).done(function(msg) {
-		var response = $.parseJSON(msg);
-
-		if(response.status == "success") {
-		    var part1 = newData.part1;
-		    var part2 = newData.part2;
-		    var part3 = newData.part3;
-
-		    let dataReplace = part1 + response.id + part2 + value + part3;
-
-		    $(prependTo).first().prepend(dataReplace);
-		    deleteForm;
-
-		    console.log("The ajax request returned successful result.");
-		} else console.log("The ajax request returned an error.");
-	    });
-	} else if(type == "edit") {
-	    e.preventDefault();
-	    var value = $(this).find('input[name='+ actionName +']').val();
-	    var id = $(this).attr(actionAttr);
-	    var actual = $(this);
-	
-	    $.ajax({
-		headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-		type: "POST",
-		url: url,
-		data: $(this).serialize()
-	    }).done(function(msg) {
-		var response = $.parseJSON(msg);
-		console.log(response.status);
-		if(response.status == "success") {
-		    var part1 = newData.part1;
-		    var part2 = newData.part2;
-		    var part3 = newData.part3;
-
-		    let dataReplace = part1 + response.id + part2 + value + part3;
-		    
-		    actual.replaceWith(dataReplace); // a corrigé, modifie tous ce qui est en cours de modification d'un coup
-		}
-	    });
-	}
-    });    
-}
-
 $(function() {
     $('.module').click(() => {
 	$('.deploy').fadeToggle(200, "linear");
@@ -101,26 +27,76 @@ $(function() {
 	});
     });
     
-    // ajout de titres
-    liveAddForm('button#add', 'ajout', 'action-add', 'nom', "Nom de la nouvelle action", '.list');
+    // TITRES - AJOUTS \\
+    // ------------------------- \\
+    $('button#add').on('click', function() {
+	var csrf_token = $('meta[name="csrf-token"]').attr('content');
+	var form = '<form method="POST" action="ajout" class="form action-add">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '">' + '<input type="text" class="form-control" name="nom" placeholder="Nom de la nouvelle action"></form>';
 
-    liveExec('add', '.action-add', 'ajout', 'nom', '', '.list', {
-	part1: '<div class="list"><div class="pull-right"><button id="edit" class="live"><span class="glyphicon glyphicon-remove" aria-hidden="true"></button></div><a href="action/',
-	part2: '"><li>',
-	part3: '</li></a></div>'
+	$('.list').first().prepend(form);
     });
 
-    // modification de titres
-    liveEditForm('button#edit', 'data-attribute', 'edit/nom', 'action-edit', 'nom', '.list');
+    $(document).on('submit', '.action-add', function (e) {
+	e.preventDefault();
+	var value = $(this).find('input[name=nom]').val();
+	var deleteForm = $(this).remove();
+	
+	$.ajax({
+	    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+	    type: "POST",
+	    url: 'ajout',
+	    data: $(this).serialize()
+	}).done(function(msg) {
+	    var response = $.parseJSON(msg);
+
+	    if(response.status == "success") {
+		let dataReplace = '<div class="list"><div class="pull-right"><button id="edit" class="live"><span class="glyphicon glyphicon-remove" aria-hidden="true"></button></div><a href="' + response.id + '"><li>' + value + '</li></a></div>';
+
+		$('.list').first().prepend(dataReplace);
+		deleteForm;
+
+		console.log("The ajax request returned successful result.");
+	    } else console.log("The ajax request returned an error.");
+	});
+    });
+
+    // TITRES - MODIFICATIONS \\
+    // ---------------------------- \\
+    $('button#edit').on('click', function() {
+	var id = $(this).attr('data-attribute');
+	var old = $(this).closest('.list').text();
+	var url = 'edit/nom';
+	var csrf_token = $('meta[name="csrf-token"]').attr('content');
+	var form = '<form method="POST" data-attribute="'+ id +'" action="' + url + '" class="form action-edit">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '">' + '<input type="hidden" name="id" value="'+ id +'"><input tfype="text" class="form-control" name="nom" value="'+ old +'"></form>';
+	$(this).closest('.list').replaceWith(form);
+    });
+
+    $(document).on('submit', '.action-edit', function (e) {
+	e.preventDefault();
+	var value = $(this).find('input[name=nom]').val();
+	var id = $(this).attr('data-attribute');
+	var actual = $(this);
+	var url = 'edit/nom';
+	
+	$.ajax({
+	    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+	    type: "POST",
+	    url: url,
+	    data: $(this).serialize()
+	}).done(function(msg) {
+	    var response = $.parseJSON(msg);
+	    console.log(response.status);
+	    if(response.status == "success") {
+		let dataReplace = '<div class="list"><div class="pull-right"><button id="edit" class="live"><span class="glyphicon glyphicon-remove" aria-hidden="true"></button></div><a href="action/' + id + '"><li>' + value + '</li></a></div>';
+		    
+		actual.replaceWith(dataReplace);
+	    }
+	});
+    });
+
+    // DESCRIPTION  - MODIFICATION \\
+    // ------------------------------ \\
     
-    liveExec('edit', '.action-edit', 'edit/nom', 'nom', 'data-attribute','.action-edit', {
-	part1: '<div class="list"><div class="pull-right"><button id="edit" class="live"><span class="glyphicon glyphicon-remove" aria-hidden="true"></button></div><a href="action/',
-	part2: '"><li>',
-	part3: '</li></a></div>'
-    });
-    // liveEditForm(element, data, url, actionName, actionAttr, replaceAt)
-    // modification de la description
-    liveEditForm('button#edit-description', 'data-attribute', 'edit/description', 'description', 'description', '.list');
 });
 
 
