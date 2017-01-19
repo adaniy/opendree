340b00a33652bd89db388833095a888dbb2748af
Daniel Medina <ifthenelse42@protonmail.com>
2017-01-19 21:44:38 +0000

    [commit #32] modification du titre et vote des budgets en cours

--- www/public/js/budget/tableau.js
+++ www/public/js/budget/tableau.js
@@ -15,14 +15,6 @@ function syncBudgetTotal(element, id) {
     });
 }
 
-// ajout de dépenses
-$('button#add').on('click', function() {
-    var csrf_token = $('meta[name="csrf-token"]').attr('content');
-    var form = '<form method="POST" action="ajout" class="form action-add">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '">' + '<input type="text" class="form-control" name="nom" placeholder="Nom de la nouvelle action"></form>';
-
-    $('.action-list').prepend(form);
-});
-
 // VALIDATION - TRAITEMENT AJAX \\
 $(document).on('click', 'button#add-depense', function (e) {
     e.preventDefault();
@@ -151,7 +143,55 @@ $(document).on('click', 'button#delete-depense', function (e) {
     });
 });
 
+// modification des budgets
+$(document).on('click', 'button#edit-budget', function() {
+    var id = $(this).parent().parent().attr('data-attribute');
 
+    var name = $(this).parent().parent();
+    var vote = $('.amount[data-attribute="'+ id +'"]');
+    var csrf_token = $('meta[name="csrf-token"]').attr('content');
+    
+    var formName = '<div class="table-header col-md-12"><form method="POST" class="form budget-name-edit" data-attribute="'+ id +'">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '"><input type="hidden" name="id" value="'+ id +'">' + '<input type="text" class="form-control" name="name" value="' + name.text() + '"></form></div>';
+    var formVote = '<td class="amount"><form method="POST" class="form budget-vote-edit" data-attribute="'+ id +'">' + '<input type="hidden" name="csrf-token" value="' + csrf_token + '"><input type="hidden" name="id" value="'+ id +'">' + '<input type="text" class="form-control" name="vote" value="'+ unformat_number(vote.text()) +'"></form</td>';
+
+    name.replaceWith(formName);
+    vote.replaceWith(formVote);
+});
+
+// traitement ajax dupliqué pour la modification des budgets
+$(document).on('submit', '.budget-name-edit', function (e) {
+    e.preventDefault();
+    var id = $(this).attr("data-attribute");
+
+    var firstForm = '.depense-category-edit';
+    var secondForm = '.depense-amount-edit';
+
+    var data1 = $('tr[data-attribute="'+ id +'"]').find(firstForm).serialize();
+    var data2 = $('tr[data-attribute="'+ id +'"]').find(secondForm).serialize();
+
+    var category = escapeHtml($('tr[data-attribute="'+ id +'"]').find('input[name=category]').val());
+    var amount = escapeHtml($('tr[data-attribute="'+ id +'"]').find('input[name=amount]').val());
+    
+    $.ajax({
+	headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+	type: "POST",
+	url: "/budget/edit/depense",
+	data: data1 + "&" + data2
+    }).done(function (msg) {
+	var response = $.parseJSON(msg);
+
+	if(response.status == "success") {
+	    // si la requête est un succès, on rétablis les colonnes avec les nouvelles valeurs asynchrone
+	    var categoryReplace = '<td class="category" data-attribute="'+ id +'">'+ response.category +'</td>';
+	    var amountReplace = '<td class="amount" data-attribute="'+ id +'">'+ response.amount +'</td>';
+
+	    $('tr[data-attribute="'+ id +'"]').find('.depense-category-edit').parent().replaceWith(categoryReplace);
+	    $('tr[data-attribute="'+ id +'"]').find('.depense-amount-edit').parent().replaceWith(amountReplace);
+
+	    syncBudgetTotal('tr.total', response.budget_id);
+	}
+    });
+});
 
 $('tr.total').each(function () {
     syncBudgetTotal('tr.total', $(this).attr('data-attribute'));
--- www/resources/views/budget/tableau.blade.php
+++ www/resources/views/budget/tableau.blade.php
@@ -1,6 +1,8 @@
+<button class="btn btn-xs btn-tree btn-default tree-category">gestion budgétaire</button>
 <ul class="tree">
+    
     @foreach($budget->groupBy('date')->get() as $budgetTree)
-    <li><button class="btn btn-tree btn-xs btn-info" type="button" data-toggle="collapse" data-target="#collapse{{ $budgetTree->date }}" aria-expanded="false" aria-controls="collapse{{ $budgetTree->date }}">{{ $budgetTree->date }}</button><button id="delete-budget" class="btn btn-xs btn-danger btn-tree"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+	<li><button class="btn btn-tree btn-xs btn-info" type="button" data-toggle="collapse" data-target="#collapse{{ $budgetTree->date }}" aria-expanded="false" aria-controls="collapse{{ $budgetTree->date }}">{{ $budgetTree->date }}</button><button id="delete-budget" class="btn btn-xs btn-danger btn-tree"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
 	    <div class="collapse" id="collapse{{ $budgetTree->date }}">
 		<ul>
 		    @foreach($service->get() as $services)
@@ -9,18 +11,12 @@
 				<ul>
 				    <li>
 					@foreach($budget->where('service_id', $services->id)->where('date', $budgetTree->date)->get() as $budgets)
-					    <button id="edit-budget" class="btn btn-xs btn-info btn-tree"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button><button id="delete-budget" class="btn btn-xs btn-danger btn-tree"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+					    <div class="table-header col-md-12" data-attribute="{{ $budgets->id }}"">{{ $budgets->name }}<div class="pull-right"><button id="edit-budget" class="btn btn-xs btn-info btn-tree"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button><button id="delete-budget" class="btn btn-xs btn-danger btn-tree"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>&nbsp;</div></div>
 					    <table class="table table-striped table-hover table-bordered table-board">
-						<tr class="budget">
-						    <th class="col-md-3">{{ $budgets->name }}</th>
-						    <th class="col-md-8">montants</th>
-						    <th class="col-md-1">actions</th>
-						</tr>
-
 						<tr class="vote">
-						    <td class="category">Budget voté</td>
-						    <td class="amount">{{ number_format($budgets->vote, 2, '.', ' ') }}</td>
-						    <td class="actions">&nbsp;</td>
+						    <td class="category col-md-5">Budget voté</td>
+						    <td class="amount col-md-6" data-attribute="{{ $budgets->id }}">{{ number_format($budgets->vote, 2, '.', ' ') }}</td>
+						    <td class="actions col-md-1">&nbsp;</td>
 						</tr>
 
 						@foreach($budgets->depenses as $depenses)
@@ -31,13 +27,12 @@
 						    </tr>
 						@endforeach
 	
-						<tr class="total" data-attribute="{{ $budgets->id }}">
+						<tr class="total table-footer" data-attribute="{{ $budgets->id }}">
 						    <td class="category">total</td>
 						    <td class="amount"><total></total></td>
-						    <td class="actions">&nbsp;</td>
+						    <td class="actions"><div class="add"><button id="add-depense" class="btn btn-md btn-warning btn-tree" data-attribute="{{ $budgets->id }}"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span></button></div></td>
 						</tr>
 					    </table>
-					    <div class="add"><button id="add-depense" class="btn btn-md btn-warning live" data-attribute="{{ $budgets->id }}"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span></button></div>
 					@endforeach
 					<li><button id="add-budget" class="btn btn-xs btn-success btn-tree"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span></button></li>
 				    </li>
@@ -49,9 +44,9 @@
 			</li>
     @endforeach
     <li><button id="add-annee" class="btn btn-xs btn-success btn-tree"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span></button></li>
-    </li>
+	</li>
 </ul>
-    </li>
+	</li>
 </ul>
 
-
+<button class="btn btn-xs btn-tree btn-default tree-category">gestion des services</button>
